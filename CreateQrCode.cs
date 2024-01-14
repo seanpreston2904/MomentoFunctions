db299e6ffbce1df7733065415d4709eb5a6ed3f7
Sean Preston <sean.preston2904@gmail.com>
2024-01-14 23:03:16 +1300

    Added CI github workflow

--- CreateQrCode.cs
+++ CreateQrCode.cs
@@ -22,7 +22,7 @@ namespace Momento.CreateQrCode
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString("Yep, this is working");
 
             return response;
         }
